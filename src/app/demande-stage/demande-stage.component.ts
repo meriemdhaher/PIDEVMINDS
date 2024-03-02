e54c997bbe9fab40e847188159be61b8531c43b2
meriemdhaher <128425485+meriemdhaher@users.noreply.github.com>
2024-03-02 03:54:55 +0100

    Commit changes

--- src/app/demande-stage/demande-stage.component.ts
+++ src/app/demande-stage/demande-stage.component.ts
@@ -1,11 +1,5 @@
 import { Component, Input } from '@angular/core';
-<<<<<<< Updated upstream
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClient } from '@angular/common/http';
-import * as jsPDF from 'jspdf';
-=======
-import { HttpClient } from '@angular/common/http';
->>>>>>> Stashed changes
 
 @Component({
   selector: 'app-demande-stage',
@@ -27,23 +21,11 @@ export class DemandeStageComponent {
       // Générez le PDF avec les informations de l'étudiant
       const pdfContent = `ID: ${this.studentInfo.Id}\nNom: ${this.studentInfo.Nom}\nPrénom: ${this.studentInfo.Prenom}\nCIN: ${this.studentInfo.Cin}\nNuméro: ${this.studentInfo.Numero}\nEmail: ${this.studentInfo.Email}\nEvenement: ${this.studentInfo.Evenement}`;
 
-<<<<<<< Updated upstream
-      // Utilisez jsPDF pour générer un PDF
-      const pdf = new jsPDF();
-      pdf.text(pdfContent, 10, 10);
-      
-      // Enregistrez ou affichez le PDF généré
-      pdf.save('demande-de-stage.pdf'); // Pour télécharger le PDF
-      // Ou affichez-le dans une nouvelle fenêtre
-      // pdf.output('dataurlnewwindow');
-
-=======
       // Logique pour générer et afficher le PDF
       console.log('PDF Content:', pdfContent);
 
       // Vous pouvez utiliser une bibliothèque ou un service pour générer le PDF ici
       // Par exemple, jspdf, pdfmake, etc.
->>>>>>> Stashed changes
     } else {
       console.log('Les informations de l\'étudiant n\'existent pas.');
     }
