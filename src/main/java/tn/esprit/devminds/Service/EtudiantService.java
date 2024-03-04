000315fc84884ba9a9576ff5e2718fb4cccb3299
meriemdhaher <128425485+meriemdhaher@users.noreply.github.com>
2024-03-04 03:46:22 +0100

    backdemandestage3

--- src/main/java/tn/esprit/devminds/Service/EtudiantService.java
+++ src/main/java/tn/esprit/devminds/Service/EtudiantService.java
@@ -5,19 +5,19 @@ import tn.esprit.devminds.Entities.Etudiant;
 import java.util.List;
 
 public interface EtudiantService {
-    // Méthode pour créer un étudiant
+
     Etudiant createEtudiant(Etudiant etudiant);
 
-    // Méthode pour modifier un étudiant
+
     Etudiant updateEtudiant(Etudiant etudiant);
 
-    // Méthode pour supprimer un étudiant
+
     void deleteEtudiant(Long id);
 
-    // Méthode pour obtenir un étudiant par son ID
+
     Etudiant getEtudiantById(Long id);
 
-    // Méthode pour obtenir tous les étudiants
+
     List<Etudiant> getAllEtudiants();
 
 
