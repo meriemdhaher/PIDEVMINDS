6bd697a117b5cc4d71bbe6bacd6e0afb0847c0b7
meriemdhaher <128425485+meriemdhaher@users.noreply.github.com>
2024-03-20 11:33:03 +0100

    commit

--- src/main/java/tn/esprit/devminds/Entities/ConventionForm.java
+++ src/main/java/tn/esprit/devminds/Entities/ConventionForm.java
@@ -1,16 +1,26 @@
 package tn.esprit.devminds.Entities;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+<<<<<<< HEAD
 import jakarta.persistence.*;
+=======
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+>>>>>>> 9e30c7eaae08c546e1d35992b01f28ff91c5e3fe
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+<<<<<<< HEAD
 import java.text.Normalizer;
 import java.util.List;
 import java.util.Set;
 
+=======
+>>>>>>> 9e30c7eaae08c546e1d35992b01f28ff91c5e3fe
 @Getter
 @Setter
 @AllArgsConstructor
@@ -27,5 +37,8 @@ public class ConventionForm {
     private String prenom;
     private Integer cin;
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> 9e30c7eaae08c546e1d35992b01f28ff91c5e3fe
 }
